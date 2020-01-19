c07e2bf9189cf8eeb677f6189b8d0f614b08e3fa
Shivam Sapate <42692084+shivamsapate@users.noreply.github.com>
2020-01-19 18:37:14 +0530

    Database file
    Program to store image in the database

---
+++ database.py
@@ -0,0 +1,45 @@
+import mysql.connector
+from mysql.connector import Error
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
+
+def insertBLOB(emp_id, name, photo, biodataFile):
+    print("Inserting BLOB into python_employee table")
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database='python_db',
+                                             user='root',
+                                             password='')
+
+        cursor = connection.cursor()
+        sql_insert_blob_query = """ INSERT INTO python_employee
+                          (id, name, photo, biodata) VALUES (%s,%s,%s,%s)"""
+
+        empPicture = convertToBinaryData(photo)
+        file = convertToBinaryData(biodataFile)
+
+        # Convert data into tuple format
+        insert_blob_tuple = (emp_id, name, empPicture, file)
+        result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
+        connection.commit()
+        print("Image and file inserted successfully as a BLOB into python_employee table", result)
+
+    except mysql.connector.Error as error:
+        print("Failed inserting BLOB data into MySQL table {}".format(error))
+
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+insertBLOB(1, "Shivam", "/home/shivam/Documents/sket5.jpg",
+           "/home/shivam/Documents/image1.txt")
+insertBLOB(2, "tushar", "/home/shivam/Documents/sket5.jpg",
+                      "/home/shivam/Documents/image1.txt")
+insertBLOB(3, "Mahesh", "/home/shivam/Pictures/mahesh.png",
+           "/home/shivam/Documents/image2.txt")
